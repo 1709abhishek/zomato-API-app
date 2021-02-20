de6a00a0847857544a48bcbea729dce69b6e98eb
abhishek.jain <abhishek.jain@avalara.com>
2021-02-20 12:47:52 +0530

    search working

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://developers.zomato.com/api/v2.1',
+  baseURL: 'https://api.yelp.com/v3/businesses',
   headers: {
-    'user-key': '132dc35ecb48fadc157488a21a3da282'
+    Authorization:
+      'Bearer MMMQuWnLGwlTYdSMtKujs774rvSF8-g78jwHgo35nIoahZ1df13ph_HxFOTGIpGD-_CVpb--RMaPA2clqd8koS8x58EPk6H9fkhW791Uws_LRizh44UyVyGh4feNX3Yx'
   }
 });
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,32 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import ResultsList from '../components/ResultsList';
 import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp';
+import useSearchResults from '../hooks/useSearchResults';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
-  const [results, setResults] = useState([]);
-  const [errorMessage, setErrorMessage] = useState('');
-
-  useEffect(() => {
-    searchApi('hich');
-  }, []);
-
-  const searchApi = async () => {
-    try {
-      const response = await yelp.get('/search', {
-        params: {
-          count: 50,
-          q: term,
-          lat: 27.1766658,
-          lon: 77.9730551
-        }
-      });
-      setResults(response.data.restaurants);
-    } catch (error) {
-      setErrorMessage('Something went wrong');
-    }
-  };
+  const [results, errorMessage, searchApi] = useSearchResults();
   return (
     <View>
       {console.log(term)}
